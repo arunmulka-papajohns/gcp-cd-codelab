4f3c19c43065b78f779c4add2faa5c4665b0fd7f
arunmulka-papajohns <arun_mulka@papajohns.com>
2018-04-16 00:13:50 -0400

    third commit

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-        io.WriteString(w, "Hello World! Welcome to GCP Spinnaker Deployment World")
+        io.WriteString(w, "Hello World! Welcome to GCP Spinnaker PROD Deployment World")
 }
 
 func main() {
